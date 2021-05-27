8a16384972cf3aaf509db0f7687511921533fe60
Krishan K Yadav <aniandp@gmail.com>
2021-05-27 14:53:55 +0530

    cleaned home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Index = () => {
       <h2>I'm Alferno, from Jaipur, Rajasthan.</h2>
       <p>
         Have a great time visiting{' '}
-        <span style={{ fontStyle: 'bold' }}>Gatsby</span> powered website.
+        <span style={{ fontStyle: 'italic' }}>Gatsby</span> powered website.
       </p>
     </Layout>
   )
